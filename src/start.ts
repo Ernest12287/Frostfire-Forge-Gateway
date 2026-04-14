a672d0c51594f95f935b31c46fb8542f776c7b54
Logan Brown <github@lilliousnetworks.com>
2026-04-14 02:08:34 -0400

    update start file

--- src/start.ts
+++ src/start.ts
@@ -1,12 +1,9 @@
-
-
-const envFile = process.env.ENV_FILE || ".env.production";
-const isDev = envFile.includes("development");
+const isDev = process.env.NODE_ENV === "development";
 
 console.log(`Starting Frostfire Forge Gateway (${isDev ? "Development" : "Production"})...\n`);
 
 console.log("Transpiling client-side TypeScript...");
-const transpileProc = Bun.spawn(["bun", `--env-file=${envFile}`, "src/utility/transpiler.ts"], {
+const transpileProc = Bun.spawn(["bun", "src/utility/transpiler.ts"], {
   stdout: "inherit",
   stderr: "inherit"
 });
@@ -20,13 +17,13 @@ if (transpileProc.exitCode !== 0) {
 console.log("Transpilation complete!\n");
 
 console.log("Starting Gateway Webserver...");
-const webserverProc = Bun.spawn(["bun", `--env-file=${envFile}`, "src/webserver/server.ts"], {
+const webserverProc = Bun.spawn(["bun", "src/webserver/server.ts"], {
   stdout: "inherit",
   stderr: "inherit"
 });
 
 console.log("Starting Gateway Server...");
-const gatewayProc = Bun.spawn(["bun", `--env-file=${envFile}`, "src/gateway/server.ts"], {
+const gatewayProc = Bun.spawn(["bun", "src/gateway/server.ts"], {
   stdout: "inherit",
   stderr: "inherit"
 });
